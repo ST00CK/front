33a05e6562e97c9614a58f01976dbf2bed6166b5
PASTELBOX <147224562+PASTELBOX@users.noreply.github.com>
2024-12-23 16:20:34 +0900

    test
    test

--- app/FriendListPage.tsx
+++ app/FriendListPage.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import MyPageButton from '../components/stoock/friendlist/MyPageButton';
+import MyPageButton from '../components/stoock/Friendlist/MyPageButton';
 
 const FriendListPage = () => {
     const navigation = useNavigation();
