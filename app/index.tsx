5063c313d9ecb05597459867d09537e4aff50900
PASTELBOX <147224562+PASTELBOX@users.noreply.github.com>
2024-12-26 13:18:39 +0900

    login component
    login component

--- app/index.tsx
+++ app/index.tsx
@@ -1,24 +1,37 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
-import { useNavigation } from 'expo-router';
+import { View, Text, Button, Image } from 'react-native';
+import { useNavigation, NavigationProp } from 'expo-router';
 import ShortButton from '../components/stoock/Common/ShortButton';
 import styles from '../styles/LoginPageStyles';
+import Input from './../components/stoock/Common/Input';
+import StoockImage from '../assets/images/STOOCK!.png';
+import kakaoImage from '../assets/images/kakao.png';
+
+type RootStackParamList = {
+    FriendListPage: undefined;
+};
 
 const LoginPage = () => {
-    const navigation = useNavigation();
+    const navigation = useNavigation<NavigationProp<RootStackParamList>>();
 
     const navigateToFriendList = () => {
         navigation.navigate('FriendListPage');
     };
 
+    const
+
     return (
         <View style={styles.container}>
-            <Text style={styles.text}>Home Tab Content</Text>
+            <Image source={StoockImage} />
+            <Text style={styles.text}>Login</Text>
             <Button title="Go to Friend List" onPress={navigateToFriendList} />
+            <Input placeholder="Email"/>
+            <Input placeholder="Password"/>
             <View style={styles.buttonContainer}>
                 <ShortButton text='Login' onClick={navigateToFriendList} />
                 <ShortButton text='Sign Up' onClick={navigateToFriendList} />
             </View>
+            <Image source={kakaoImage} />
         </View>
     );
 };
