59ee990eceb225e20f914e25b2273d565f82373c
TreeBomb-01 <150654998+TreeBomb-01@users.noreply.github.com>
2025-02-13 14:25:04 +0900

    비밀번호 변경시 이메일 인증 수정

--- app/PasswdChangePage.tsx
+++ app/PasswdChangePage.tsx
@@ -67,7 +67,7 @@ const PasswdChangePage = () => {
 
         try {
             const response = await emailCheckMutation.mutateAsync({ email, authCode: inputEmailCode });
-            if (response.message === "인증이 성공적으로 완료되었습니다.") {
+            if (response === "인증이 성공적으로 완료되었습니다.") {
                 setIsEmailVerified(true);
                 alert("이메일 인증이 완료되었습니다.");
                 Animated.timing(passwordSlideAnim, {
