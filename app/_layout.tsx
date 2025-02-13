4e53f24bf7d2283675dcb6dac78e38e4c07d3306
TreeBomb-01 <150654998+TreeBomb-01@users.noreply.github.com>
2025-02-13 14:24:48 +0900

    카카오이니셜라이즈 추가

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,7 @@
 import { Stack } from 'expo-router';
 import React from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import KakaoInitializer from '../components/KakaoInitializer';
 
 // QueryClient 생성
 const queryClient = new QueryClient();
@@ -8,6 +9,7 @@ const queryClient = new QueryClient();
 const Layout = () => {
     return (
         <QueryClientProvider client={queryClient}>
+            <KakaoInitializer />
             <Stack
                 initialRouteName="LoginPage"
                 screenOptions={{
